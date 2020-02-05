5cfc4092614611120ea200ceaeb520f052efa9ff
Lance Henderson <jecnal118@gmail.com>
2020-02-05 12:32:34 -0500

    Removed moment.js and fixed refernece to BO (business object) reference

---
+++ Bball.BAL/AdjustmentsBO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using BballMVC.DTOs;
+using Bball.DAL.Tables;
+
+namespace Bball.BAL
+{
+    public class AdjustmentsBO
+    {
+        public List<AdjustmentDTO> GetTodaysAdjustments(string LeagueName)
+        {
+            Bball.DAL.Tables.AdjustmentsDO oAdjustments = new Bball.DAL.Tables.AdjustmentsDO();
+            return oAdjustments.GetTodaysAdjustments(LeagueName);
+
+        }
+    }
+}
